78ea7b5d23b6e0c3e059ef5fc9d926d02f1169be
s <s>
2022-09-07 18:36:41 +0500

    Введение в JavaScript: Идентификаторы: переменные и константы

---
+++ 02-naming.js
@@ -0,0 +1,10 @@
+const MAX_PURCHASE = 2000;
+
+const calculateTotal = (prices) => {
+  let amount = 0;
+  if (!Array.isArray(prices)) throw 'Array expected';
+  for (const price of prices) {
+    amount += price;
+  }
+  return amount;
+};
