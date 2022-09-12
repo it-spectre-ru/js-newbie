c150d5c12c0de83f8f7775fbb8269b0ac81513d6
s <s>
2022-09-12 12:17:23 +0500

    if statement

---
+++ 05-if.js
@@ -0,0 +1,8 @@
+const calculateSubtotal = (goods) => {
+  let amount = 0;
+  for (const item of goods) {
+    if (item.price < 0) throw 'Negative price';
+    else amount += item.price;
+  }
+  return amount;
+};
