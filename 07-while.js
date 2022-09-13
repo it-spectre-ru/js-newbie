607bff6b48061b09f1863d24f060944a83253873
s <s>
2022-09-13 16:09:51 +0500

    real example  refactor with while

--- 07-while.js
+++ 07-while.js
@@ -1,8 +1,9 @@
 const sum = (...args) => {
+  if (args.length === 0) return 0;
   let value = 0;
-  while (args.length > 0) {
+  do {
     value += args.pop();
-  }
+  } while (args.length > 0);
   return value;
 };
 
