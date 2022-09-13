d6eb74face80f430b2a632cab9f1456be829be02
s <s>
2022-09-13 16:15:01 +0500

    function sleep

--- 07-while.js
+++ 07-while.js
@@ -1,10 +1,8 @@
-const sum = (...args) => {
-  if (args.length === 0) return 0;
-  let value = 0;
-  do {
-    value += args.pop();
-  } while (args.length > 0);
-  return value;
+const now = (date = new Date()) => date.getTime();
+
+const sleep = (interval) => {
+  const end = now() + interval;
+  while (now() < end);
 };
 
-console.log(sum(1, 2, 3, 4, 5));
+sleep(5000);
