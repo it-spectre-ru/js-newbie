7b46dcdc94effa8c6b6f8ad355aad97c8a9ce79e
s <s>
2022-09-14 12:34:15 +0500

    init array

---
+++ 08-array.js
@@ -0,0 +1,8 @@
+const purchase = {
+  Electronics: [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Keyboard', price: 500 },
+    { name: 'HDMI', price: 10 },
+  ],
+  Texttile: [{ name: 'Bag', price: 50 }],
+};
