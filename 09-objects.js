9fd0c01fd575a85870c759ca4b503e2657978743
s <s>
2022-09-21 20:03:03 +0500

    input in console

--- 09-objects.js
+++ 09-objects.js
@@ -1,20 +1,10 @@
-const basket = {
-  Laptop: 1500,
-  Keyboard: 100,
+const purchase = {
+  Electronics: [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Keyboard', price: 100 },
+    { name: 'HDMI cable', price: 10 },
+  ],
+  Textile: [{ name: 'Bag', price: 50 }],
 };
 
-const calcalateTotal = (obj) => {
-  let total = 0;
-  for (const key in obj) {
-    total += obj[key];
-  }
-  return total;
-};
-
-const goodsTotal = (goods) => {
-  return { goods, total: calcalateTotal(goods) };
-};
-
-const order = goodsTotal(basket);
-
-console.log(order);
+console.log({ purchase });
