dbbd866337b6d2cde5ec2f0394364be87ced6af2
s <s>
2022-09-22 19:03:26 +0500

    Example - 2 solution
    1 input summ

--- 10-task.js
+++ 10-task.js
@@ -1,20 +1,11 @@
-const items = [{ price: 40 }, { price: 120 }, { price: 505 }, { price: 350 }];
+const items = [{ price: 40 }, { price: -120 }, { price: '505' }, { price: 350 }];
 
-const calculateTotal = (obj) => {
-  let summ = 0;
-  items.forEach((item, index, array) => {
-    if (item['price'] > 0) {
-      summ += item['price'];
-    }
-    console.log(item['price']);
-    console.log(index);
-  });
+const calculateTotal = (goods) => {
+  let total = 0;
+  for (const item of goods) {
+    total += item.price;
+  }
+  return total;
 };
 
 console.log(calculateTotal(items));
-
-// console.log(items[0]['price']);
-
-// for (const item of items) {
-//   console.log(`Price: ${item.price}`);
-// }
