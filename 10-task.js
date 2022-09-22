9c8a166dbc59fac7db9d5db68e6fb921e2aef0f7
s <s>
2022-09-22 19:08:13 +0500

    Example - 2 solution
    2 check typeof

--- 10-task.js
+++ 10-task.js
@@ -3,7 +3,9 @@ const items = [{ price: 40 }, { price: -120 }, { price: '505' }, { price: 350 }]
 const calculateTotal = (goods) => {
   let total = 0;
   for (const item of goods) {
-    total += item.price;
+    if (typeof item.price === 'number') {
+      total += item.price;
+    }
   }
   return total;
 };
