2536af485d71403829fa4cf88cebf25109170841
s <s>
2022-09-22 19:00:00 +0500

    Example - 2 solution
    my

--- 10-task.js
+++ 10-task.js
@@ -1,8 +1,13 @@
 const items = [{ price: 40 }, { price: 120 }, { price: 505 }, { price: 350 }];
 
 const calculateTotal = (obj) => {
+  let summ = 0;
   items.forEach((item, index, array) => {
+    if (item['price'] > 0) {
+      summ += item['price'];
+    }
     console.log(item['price']);
+    console.log(index);
   });
 };
 
