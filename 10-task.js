17fbfa2c9f017663af3303fc4a2637cc1b4166d2
s <s>
2022-09-22 19:16:36 +0500

    refactoring 4

--- 10-task.js
+++ 10-task.js
@@ -3,9 +3,8 @@ const items = [{ price: 40 }, { price: -120 }, { price: '505' }, { price: 350 }]
 const calculateTotal = (goods) => {
   let total = 0;
   for (const { price } of goods) {
-    if (typeof price === 'number' && price > 0) {
-      total += price;
-    }
+    if (typeof price !== 'number') continue;
+    if (price > 0) total += price;
   }
   return total;
 };
