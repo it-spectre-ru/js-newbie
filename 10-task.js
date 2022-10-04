1120034a06cc0a33710c638defa8fb2442ff41f6
it-spectre-ru <it-spectre-ru@gmail.com>
2022-10-04 21:18:25 +0500

    refactoring 4 self 3 reduce

--- 10-task.js
+++ 10-task.js
@@ -1,5 +1,14 @@
-const items = [{ price: 250 }, { price: -150 }, { price: 150 }, { price: 500 }];
+const items = [{ price: -50 }, { price: 50 }, { price: '100' }, { price: 100 }];
 
-let result = items.map(({ price }) => price);
+// let result = items.map(({ price }) => {
+//   price;
+//   if (price > 0) console.log(price);
+// });
 
-console.log(result);
+const calculateTotal = items.reduce((total, item) => {
+  if (item.price > 0 && typeof item.price === 'number') return (total += item.price);
+  return total;
+}, 0);
+
+console.log(calculateTotal);
+// console.log(result);
