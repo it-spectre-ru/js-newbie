ce2503f469fe65da2d5510524242b9af02822fd5
s <s>
2022-11-03 17:01:24 +0500

    Intersection

--- 11-set-map.js
+++ 11-set-map.js
@@ -1,6 +1,6 @@
-const goods = new Set(['Laptop', 'Keyboard', 'Mouse']);
-const added = new Set(['Textile', 'Bag']);
+const order = new Set(['Laptop', 'Keyboard', 'Mouse']);
+const stock = new Set(['Textile', 'Keyboard']);
 
-const basket = new Set([...goods, ...added]);
+const basket = new Set([...order].filter((item) => stock.has(item)));
 
 console.log(basket);
