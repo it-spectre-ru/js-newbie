bc182d175399d89a280e86787aeabc56f1e386e6
s <s>
2022-11-03 17:09:04 +0500

    Read

--- 11-set-map.js
+++ 11-set-map.js
@@ -1,7 +1,7 @@
-const goods = new Map();
-goods.set('Laptop', 1500);
-goods.set('Keyboard', 100);
+const electronics = new Map();
+electronics.set('Laptop', 1500);
+electronics.set('Keyboard', 100);
 
-for (const [key, value] of goods) {
-  console.log(`Price of ${key} is ${value}`);
-}
+const price = electronics.get('Laptop');
+
+console.log({ price });
