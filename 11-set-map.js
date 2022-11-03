1d68e98447a10b27fe3098f5318fba811dd104f4
s <s>
2022-11-03 17:11:31 +0500

    hash table from object

--- 11-set-map.js
+++ 11-set-map.js
@@ -1,7 +1,7 @@
-const goods = new Map();
-goods.set('Laptop', 1500);
-goods.set('Keyboard', 100);
+const electronics = {
+  Laptop: 1500,
+  Keyboard: 100,
+};
 
-if (goods.has('Laptop')) {
-  console.log('We have a Laptop');
-}
+const goods = new Map(Object.entries(electronics));
+console.log(goods);
